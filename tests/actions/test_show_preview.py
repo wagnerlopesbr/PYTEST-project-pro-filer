44a321a107e1808f08c2e6b1c85e9347ee42e868
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-05-01 19:20:55 -0300

    DONE: Req 3/6 v1

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,1 +1,41 @@
 from pro_filer.actions.main_actions import show_preview  # NOQA
+import pytest
+from typing import Dict, List, Tuple
+
+
+@pytest.mark.parametrize(
+    "context, expected_output",
+    [
+        (
+            {
+                "all_files": [
+                    "src/__init__.py", "src/app.py", "src/utils/__init__.py"
+                    ],
+                "all_dirs": ["src", "src/utils"]
+            },
+            "First 5 files: ['src/__init__.py'" +
+            ", 'src/app.py', 'src/utils/__init__.py']\n"
+            "First 5 directories: ['src', 'src/utils']\n"
+        ),
+        (
+            {"all_files": [], "all_dirs": []},
+            "Found 0 files and 0 directories\n"
+        ),
+        (
+            {"all_files": ["file1.txt"], "all_dirs": []},
+            "First 5 files: ['file1.txt']\n"
+        ),
+        (
+            {"all_files": [], "all_dirs": ["dir1", "dir2"]},
+            "First 5 directories: ['dir1', 'dir2']\n"
+        ),
+    ]
+)
+def test_show_preview(
+        context: Dict[str, List[str]],
+        expected_output: str,
+        capsys: Tuple[str, str],
+        ) -> None:
+    show_preview(context)
+    captured = capsys.readouterr()
+    assert expected_output.strip() in captured.out.strip()
