e4c1a7be60f09b3f4361cc59ca82588eb95e6b34
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-05-02 13:04:42 -0300

    DONE: Req6/6 v1

--- tests/actions/test_find_duplicate_files.py
+++ tests/actions/test_find_duplicate_files.py
@@ -1,1 +1,28 @@
 from pro_filer.actions.main_actions import find_duplicate_files  # NOQA
+import pytest
+
+
+def test_find_duplicate_files(tmp_path):
+    files = []
+    file_1 = tmp_path / "file_test1.txt"
+    with open(file_1, "w") as f:
+        f.write("GABIGOL VOLTOU")
+
+    file_2 = tmp_path / "file_test2.txt"
+    with open(file_2, "w") as f:
+        f.write("MAS AINDA NÃO FEZ GOL")
+
+    file_2_2 = tmp_path / "file_test2_2.txt"
+    with open(file_2_2, "w") as f:
+        f.write("MAS AINDA NÃO FEZ GOL")
+
+    files.append(str(file_1))
+    files.append(str(file_2))
+    files.append(str(file_2_2))
+    to_check = find_duplicate_files({"all_files": files})
+    assert len(to_check) == 1
+    assert len(to_check[0]) == 2
+
+    files.append("inexistent_file.txt")
+    with pytest.raises(ValueError, match="All files must exist"):
+        to_check = find_duplicate_files({"all_files": files})
